85cf95951fd06a0450c70ec150d78e4a6eb0b300
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-26 00:01:01 +0330

    chore: static settings added

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cafe.urls', namespace='cafe')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
