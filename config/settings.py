22e5de0fdf0938ca16e3b25d6787e8080bcf12bf
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-25 23:42:03 +0330

    chore: apps added in INSTALLED_APPS list

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cafe.apps.CafeConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
