421de809731699ce22b01d51cab0f331a0c6999f
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-27 04:35:59 +0330

    feat: Implement Order model in cafe app.

--- cafe/models.py
+++ cafe/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Order(models.Model):
+    table_name = models.IntegerField(null=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+    total_price_= models.FloatField()
+
+    # Foreign keys
+    customer = models.ManyToManyField(User, through= 'OrderItem')
+    personnel= models.ManyToManyField(User, through='OrderItem', null=True)
