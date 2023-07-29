11e12c8766de581d6c273997563927f91316bceb
HosseinTahami <hosseintahami2000@gmail.com>
2023-07-29 19:06:30 +0330

    chore: 'login/' url added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import UserLoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
+    
+    path('login/', UserLoginView.as_view(), name='login'),
     
 ]
