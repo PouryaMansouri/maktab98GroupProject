2bdbf26175cd8cd5d4ad82f2ec22026aff8757a7
mabrishami00 <mabrishami78@yahoo.com>
2023-07-29 15:15:42 +0330

    feat:write Personnel model instead of user django model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
 
 # Create your models here.
+class Personnel(AbstractBaseUser):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=11, unique=True)
+    image = models.ImageField(upload_to="personnel/", blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["email", "full_name"]
+
+    def __str__(self):
+        return self.email
+
+    def has_perms(self, perm_list, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
