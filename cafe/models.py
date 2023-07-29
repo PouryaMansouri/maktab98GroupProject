c526e63106bbb234f3f8ed145b90fac16f9e53e6
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-29 09:05:20 +0330

    chore: Define Personnel class and customer models and relate foreign keys of Order to them.

--- cafe/models.py
+++ cafe/models.py
@@ -1,16 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
 # Create your models here.
+
+class Personnel(models.Model):
+    pass
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
 
 class Order(models.Model):
     table_name = models.IntegerField(null=True)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True, null=True)
-    total_price_= models.FloatField()
+    total_price = models.FloatField()
 
     # Foreign keys
-    customer = models.ManyToManyField(User)
-    personnel= models.ManyToManyField(User)
+    customer = models.ForeignKey(Customer, on_delete= models.PROTECT)
+    personnel= models.ForeignKey(Personnel, null=True, on_delete= models.PROTECT)
 
 class Category(models.Model):
     pass
