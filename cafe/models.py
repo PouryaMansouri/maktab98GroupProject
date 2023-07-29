385a790eb5a32de7d78657271c71383ab38e07b6
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-07-29 07:55:50 +0330

    fix: Edit a mistake in defining relations.

--- cafe/models.py
+++ cafe/models.py
@@ -9,9 +9,8 @@ class Order(models.Model):
     total_price_= models.FloatField()
 
     # Foreign keys
-    # OrderItem will be bridge table between Order and Product
-    customer = models.ManyToManyField(User, through= 'OrderItem')
-    personnel= models.ManyToManyField(User, through='OrderItem', null=True)
+    customer = models.ManyToManyField(User)
+    personnel= models.ManyToManyField(User)
 
 class Category(models.Model):
     pass
