13a11f00a7937e926ab03033785fba41056187c1
your_github_username <your_github_email>
2023-07-30 22:13:14 +0330

    feat: make iterabel class

--- orders/views.py
+++ orders/views.py
@@ -9,6 +9,16 @@ class CartView(View):
     def get(self, request):
         return render(request, 'orders/cart.html')
 
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['total_price'] = int(item['price'])*  item['quantity']
+        yield item 
+
 
 class CartAddView(View):
     def post(self, request, product_id):
