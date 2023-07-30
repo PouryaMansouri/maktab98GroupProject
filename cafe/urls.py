75da81dc2c5e84080f370d30b241b4ded2b61646
AnahitaDzl <anahita.dozali@gmail.com>
2023-07-30 20:42:00 +0330

    feat: gallery added

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import Menu
+
 app_name = 'cafe'
 
 urlpatterns = [
--- cafe/views.py
+++ cafe/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def Menu(request):
     context = {}
-    return render(request, 'menu.html', context)
+    return render(request, 'cafe\menu.html', context)
