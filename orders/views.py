c5f9762f698b7d41228bb1f3ae27cfac8c137ec4
your_github_username <your_github_email>
2023-07-30 22:03:27 +0330

    feat: make cartaddview class

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect 
 from django.views import View
+from .cart import Cart
+from home.models import Product
+from .forms import CartAddForm
 
-# Create your views here.
 
 class CartView(View):
-    def get(self,request):
+    def get(self, request):
         return render(request, 'orders/cart.html')
+
+
+class CartAddView(View):
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404 (Product, id=product_id)
+        form = CartAddForm (request.POST) 
+        if form.is_valid():
+            cart.add(product,form.cleaned_data['quantity'])
+        return redirect('orders: cart')
+
