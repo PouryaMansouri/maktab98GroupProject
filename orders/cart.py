33139646f204aa2dcd36bb29abae7c2e5c34dfc4
your_github_username <your_github_email>
2023-07-30 21:35:40 +0330

    feat: make class cart

--- orders/cart.py
+++ orders/cart.py
@@ -1,1 +1,12 @@
+CART_SESSION_ID='cart'
 
+class cart:
+    def __init__(self, request):
+        self.session = request.session
+        cart=self.session.get(CART_SESSION_ID)
+        if not cart :
+            cart=self.session[CART_SESSION_ID]={}
+        self.cart=cart
+
+    def ass (self, product , quantity):
+        product_id=str(product.id)
