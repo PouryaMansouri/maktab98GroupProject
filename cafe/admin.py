1c97806615e1ed37902f50c9e4338b1c44486809
your_github_username <your_github_email>
2023-07-31 23:20:52 +0330

    feat:register in admin

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category,Product ,Customer
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Customer)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
