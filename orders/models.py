f310cf617a70c51e8caac980faab20896e28fc40
mabrishami00 <mabrishami78@yahoo.com>
2023-07-31 11:16:57 +0330

    refactor: transfer order related models to order app

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from cafe.models import Customer, Product, Personnel
 # Create your models here.
+class Order(models.Model):
+    table_name = models.IntegerField(null=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True, null=True)
+    total_price = models.FloatField()
+
+    # Foreign keys
+    customer = models.ForeignKey(Customer, on_delete= models.PROTECT)
+    personnel= models.ForeignKey(Personnel, null=True, on_delete= models.PROTECT)
+
+class OrderItem(models.Model):
+    quantity = models.IntegerField()
+    
+    # Foreign keys
+    order = models.ForeignKey(Order, on_delete= models.CASCADE)
+    product = models.ForeignKey(Product, on_delete= models.CASCADE)
