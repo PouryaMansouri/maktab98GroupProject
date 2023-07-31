f19dc056152517c3200637c58acd14dbd1726852
your_github_username <your_github_email>
2023-08-01 00:41:12 +0330

    feat:make cafe menu

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import Menu
-from .views import HomeView
+from .views import HomeView ,ProductDetail , ProductDetail2
 
 app_name = 'cafe'
 
 urlpatterns = [
     path('', Menu , name="menu"),
     path('home/', HomeView.as_view(), name='home'),
+    path('product/' , ProductDetail.as_view() , name="product"),
+    path('product_detail/<int:pk>/', ProductDetail2.as_view() , name="product_detail"),
 ]
--- cafe/views.py
+++ cafe/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Product
 
 # Create your views here.
 def Menu(request):
+    products = Product.objects.all()
     context = {}
-    return render(request, 'cafe/menu.html', context)
+    return render(request, 'cafe/menu.html',  {'products':products})
 
 class HomeView(View):
     def get(self, request):
@@ -12,3 +14,14 @@ class HomeView(View):
 
     def post(self, request):
         return render(request, 'home/index.html')
+    
+class ProductDetail(View):
+    def get(self , request):
+        products = Product.objects.filter(is_available = True)
+        return render(request, 'cafe/product.html' , {'products':products})
+
+
+class ProductDetail2(View):
+    def get(self , request , pk):
+        productd = Product.objects.get(pk=pk)
+        return render(request, 'cafe/product_detail.html' , {'productd':productd})
