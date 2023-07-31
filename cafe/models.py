82e8aaaf810918eed6c2f78ffa006a063363e770
mabrishami00 <mabrishami78@yahoo.com>
2023-07-31 11:19:23 +0330

    refactor: transfer and complete personnel model

--- cafe/models.py
+++ cafe/models.py
@@ -1,29 +1,23 @@
 from django.db import models
 from utils import item_directory_path
 
-# Create your models here.
-
-class Personnel(models.Model):
-    pass
 
 class Customer(models.Model):
     name = models.CharField(max_length=255, null=True)
     phone_number = models.CharField(max_length=20, unique=True)
 
 
-
-
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to = item_directory_path)
+    image = models.ImageField(upload_to=item_directory_path)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to = item_directory_path)
-    is_available = models.BooleanField(default= True)
+    image = models.ImageField(upload_to=item_directory_path)
+    is_available = models.BooleanField(default=True)
     description = models.TextField()
     price = models.FloatField()
 
     # Foreign keys
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
-
