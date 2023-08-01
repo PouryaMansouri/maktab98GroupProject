9fc628db024edbc28f2974cc04937ecd3aa77607
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-01 11:54:44 +0330

    fix: home --> cafe in template direction

--- cafe/views.py
+++ cafe/views.py
@@ -4,17 +4,17 @@ from .models import Category, Product
 
 # Create your views here.
 def Menu(request):
-    context = {}
-    return render(request, 'cafe/home.html', context)
     all_categories = Category.objects.all()
     all_products = Product.objects.all()
-
     context = {'all_categories': all_categories, 'all_products': all_products}
-    return render(request, 'cafe/menu.html', context)
+    return render(request, 'cafe/home.html', context)
 
 class HomeView(View):
     def get(self, request):
         return render(request, 'cafe/home.html')    
 
     def post(self, request):
-        return render(request, 'home/home.html')
+        return render(request, 'cafe/home.html')    
+
+    def post(self, request):
+        return render(request, 'cafe/home.html')
