1b2e7cf96fbf7c327c7395734f63b3b59a140e81
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-08-02 15:50:57 +0330

    fix: remove bugs

--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ def item_directory_path(instance, filename):
     # so need to get next availabe id from db
     # also, instance has not access to its class, so use __class__:
 
-    users = instance.__class__.objects.all()
+    items = instance.__class__.objects.all()
     #might be possible model has no records so make instance.__class__ to handle None
-    next_id = users.aggregate(Max('id'))['id__max'] + 1 if users else 1
+    next_id = items.aggregate(Max('id'))['id__max'] + 1 if items else 1
     # file will be uploaded to MEDIA_ROOT / <instance.__class__.__name__>_<id>/<filename>
     return '{2}_{0}/{1}'.format(next_id, filename, instance.__class__.__name__)
