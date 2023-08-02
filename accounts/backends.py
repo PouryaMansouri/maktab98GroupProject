6bbda5042cc64234dbf47473448df5d9530beee9
mabrishami00 <mabrishami78@yahoo.com>
2023-08-02 22:29:18 +0330

    refactor: clean coding

--- accounts/backends.py
+++ accounts/backends.py
@@ -2,23 +2,20 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import BaseBackend
 
 
-
-
 User = get_user_model()
 
+
 class PhoneBackend(BaseBackend):
-    def authenticate(self, request,  phone_number=None):
+    def authenticate(self, request, phone_number=None):
         try:
             user = User.objects.get(phone_number=phone_number)
         except User.DoesNotExist:
-            return None 
+            return None
         return user
-    
+
     def get_user(self, user_id: int):
         try:
             user = User.objects.get(pk=user_id)
         except:
             return None
         return user
-        
- 
