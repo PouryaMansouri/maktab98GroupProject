800ef92956086a3924853dda64c6db9c2cce6621
your_github_username <your_github_email>
2023-08-02 22:50:01 +0330

    feat:make model for orders

--- orders/models.py
+++ orders/models.py
@@ -1,20 +1,40 @@
 from django.db import models
 from cafe.models import Product
 from accounts.models import Personnel, Customer
+from cafe.models import Customer, Product
+from accounts.models import Personnel
+
 # Create your models here.
 class Order(models.Model):
     table_name = models.IntegerField(null=True)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True, null=True)
     total_price = models.FloatField()
+    paid=models.BooleanField(default=False)
 
     # Foreign keys
     customer = models.ForeignKey(Customer, on_delete= models.PROTECT)
     personnel= models.ForeignKey(Personnel, null=True, on_delete= models.PROTECT)
 
+    class Meta:
+        ordering = ('total_price' , '-update_time')
+
+    def __str__(self):
+        return f'{self.user} - {self.id}'
+    
+    def get_total_price(self):
+        return sum(item.get_cost() for item in self.items.all())
+
 class OrderItem(models.Model):
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(default=1)
     
     # Foreign keys
-    order = models.ForeignKey(Order, on_delete= models.CASCADE)
+    order = models.ForeignKey(Order, on_delete= models.CASCADE , related_name='item')
     product = models.ForeignKey(Product, on_delete= models.CASCADE)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.id
+    
+    def get_cost(self):
+        return self.price * self.quantity
