ad52fc539a2b0f445361f7055d19e472c584335c
mabrishami00 <mabrishami78@yahoo.com>
2023-08-02 21:55:20 +0330

    chore: fix links

--- cafe/views.py
+++ cafe/views.py
@@ -4,11 +4,11 @@ from django.views import View
 # Create your views here.
 def Menu(request):
     context = {}
-    return render(request, 'cafe/menu.html', context)
+    return render(request, 'cafe/home.html', context)
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/index.html')    
+        return render(request, 'cafe/home.html')    
 
     def post(self, request):
-        return render(request, 'home/index.html')
+        return render(request, 'home/home.html')
