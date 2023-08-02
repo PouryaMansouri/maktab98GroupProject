39f37e955232818cc4f9f0e190601823a86ea353
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-08-02 15:24:01 +0330

    chore: Insert categories and products to context

--- cafe/views.py
+++ cafe/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Category, Product
 
 # Create your views here.
 def Menu(request):
     context = {}
     return render(request, 'cafe/home.html', context)
+    all_categories = Category.objects.all()
+    all_products = Product.objects.all()
+
+    context = {'all_categories': all_categories, 'all_products': all_products}
+    return render(request, 'cafe/menu.html', context)
 
 class HomeView(View):
     def get(self, request):
