eefb83999226e889363298f4fff135ad3dd73eac
mabrishami00 <mabrishami78@yahoo.com>
2023-08-05 19:28:54 +0330

    chore: add verify personnel url

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('verify_personnel/', views.UserVerifyPersonnelView.as_view(), name='verify_personnel'),
 ]
