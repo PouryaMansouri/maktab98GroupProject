50974f6aadd3c9f6c8967c60e0060d30b722ac39
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-08-06 11:18:39 +0330

    chore: update fields of Order model.

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-from cafe.models import Product
-from accounts.models import Personnel, Customer
+from cafe.models import Customer, Product
+from accounts.models import Personnel
 # Create your models here.
 class Order(models.Model):
+    STATUS_FIELDS = [('p', 'Pending'),('a','Aaccepted'),('r','Rejected')]
     table_name = models.IntegerField(null=True)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True, null=True)
     total_price = models.FloatField()
+    status = models.CharField(max_length=1, choices= STATUS_FIELDS, default= 'p')
 
     # Foreign keys
     customer = models.ForeignKey(Customer, on_delete= models.PROTECT)
@@ -14,6 +16,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     
     # Foreign keys
     order = models.ForeignKey(Order, on_delete= models.CASCADE)
