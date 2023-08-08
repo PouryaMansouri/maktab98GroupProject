46e08880f9214955d9a14d183b24f7a1737ec60b
your_github_username <your_github_email>
2023-08-08 21:53:43 +0330

    feat:change admin panel

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Category,Product ,Customer
+
+# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display =('name' ,)
+    search_fields = ('name' , )
+
+@admin.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display =('name' , 'price' , 'is_available' , 'category')
+    list_filter = ('is_available' ,'category')
+    search_fields = ('name' , 'price')
+    list_editable= ('is_available','price')
+
+@admin.register(Customer)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display =('name' , 'phone_number')
+    search_fields = ('name' , )
