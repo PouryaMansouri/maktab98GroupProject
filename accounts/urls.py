2d7e50825b97455ec86b0ab552d5f7bc72e84e0d
AnahitaDzl <anahita.dozali@gmail.com>
2023-08-09 19:02:18 +0330

    fix: order managment view updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('verify_personnel/', views.UserVerifyPersonnelView.as_view(), name='verify_personnel'),
-    path('ManageOrders/' , views.ManageOrders.as_view() , name='ManageOrders'),
+    path('manage_orders/' , views.ManageOrders.as_view() , name='manage_orders'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 from utils import send_otp_code
-from .models import OTPCode
+from .models import OTPCode 
+from orders.models import Order
 from .forms import UserCustomerLoginForm, OTPForm
 
 from random import randint
@@ -84,7 +85,7 @@ class UserVerifyPersonnelView(View):
                 if user is not None:
                     login(request, user)
                     messages.success(request, "Logged in Successfully", "success")
-                    return redirect("cafe:home")
+                    return redirect("accounts:manage_orders")
                 else:
                     messages.error(
                         request, "The code or phone_number is wrong!", "error"
@@ -102,4 +103,5 @@ class UserLogoutView(View):
 
 class ManageOrders(View):
     def get(self , request):
-        return render(request, 'accounts/manage_orders.html')
+        orders = Order.objects.all()
+        return render(request, 'accounts/manage_orders.html', {'orders': orders})
