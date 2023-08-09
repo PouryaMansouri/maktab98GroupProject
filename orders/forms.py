7b09fc7c0d4ab7c1e30a1862203ca2d295d81609
mabrishami00 <mabrishami78@yahoo.com>
2023-08-09 15:40:06 +0330

    feat: add customer form

--- orders/forms.py
+++ orders/forms.py
@@ -8,3 +8,7 @@ class CartAddForm(forms.Form):
         widget=forms.NumberInput(attrs={"class": "form-control"}),
         initial=1
     )
+
+class CustomerForm(forms.Form):
+    phone_number = forms.CharField(max_length=11, widget=forms.TextInput)
+    table_number = forms.IntegerField()
