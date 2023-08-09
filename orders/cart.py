1fefabb35a3cbe322e71eab076dda043c201c0c9
mabrishami00 <mabrishami78@yahoo.com>
2023-08-09 14:44:28 +0330

    feat: get cookie and adding item to it methods

--- orders/cart.py
+++ orders/cart.py
@@ -1,45 +1,30 @@
+from django.shortcuts import redirect
+
 from cafe.models import Product
 
-CART_SESSION_ID='cart'
+import json
+
+
+CART_COOKIE_KEY = "cart"
+
 
 class Cart:
-    def __init__(self, request):
-        self.session = request.session
-        cart=self.session.get(CART_SESSION_ID)
-        if not cart :
-            cart=self.session[CART_SESSION_ID]={}
-        self.cart=cart
+    def __init__(self, request) -> None:
+        cart = request.COOKIES.get(CART_COOKIE_KEY)
+        if not cart:
+            self.cart = {}
+        else:
+            self.cart = json.loads(cart)
+
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        if not product_id in self.cart:
+            self.cart[product_id] = {
+                "product": product.name,
+                "quantity": quantity,
+            }
+        else:
+            self.cart[product_id]["quantity"] += quantity
 
 
-    def __iter__(self):
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
-        cart = self.cart.copy()
-        for product in products:
-            cart[str(product.id)]['product'] = product
 
-        for item in cart.values():
-            item['total_price'] = float(item['price'])*  item['quantity']
-            yield item     
-    
-    def __len__(self):
-        return sum(item['quantity'] for item in self.cart.values())
-    
-    def add (self, product , quantity):
-        product_id=str(product.id)
-        if product_id not in self.cart:
-            self.cart[product_id]={'quantity':0 , 'price':str(product.price)}
-        self.cart[product_id]['quantity'] += quantity
-        self.save()
-
-    def remove(self , product):
-        product_id=str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
-            self.save()
-
-    def save(self):
-        self.session.modified = True
-
-    def get_total_price(self):
-        return sum(float(item['price'])* item['quantity'] for item in self.cart.values())
