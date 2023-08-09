9e9a55878e21a27e85b4eb94a25f107e956551a9
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-09 22:15:54 +0330

    refactor: Better names for urls

--- orders/urls.py
+++ orders/urls.py
@@ -2,18 +2,19 @@ from django.urls import path
 from . import views
 
 
-app_name='orders'
+app_name = "orders"
 
 urlpatterns = [
-    path('add_order/', views.AddOrderView.as_view(), name='add_order'),
-    path('detail/' , views.OrderDetailView.as_view() , name='order_detail'),
-    path('cart/', views.CartView.as_view(),name='cart'),
-    path('cart/add/<int:product_id>/', views.CartAddView.as_view() , name="cart_add"),
-    path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view() , name="cart_remove"),
-    path('<int:pk>/accept/', views.order_accept, name='order_accept'),
-    path('<int:pk>/reject/', views.order_reject, name='order_reject'),
-    path('checkout/' , views.CheckoutView.as_view() , name='checkout'),
+    path("add_order/", views.AddOrderView.as_view(), name="add_order"),
+    path("order_detail/", views.OrderDetailView.as_view(), name="order_detail"),
+    path("cart/", views.CartView.as_view(), name="cart"),
+    path("cart_add/<int:product_id>/", views.CartAddView.as_view(), name="cart_add"),
+    path(
+        "cart_remove/<int:product_id>/",
+        views.CartRemoveView.as_view(),
+        name="cart_remove",
+    ),
+    path("accept/<int:pk>", views.order_accept, name="order_accept"),
+    path("reject/<int:pk>", views.order_reject, name="order_reject"),
+    path("checkout/", views.CheckoutView.as_view(), name="checkout"),
 ]
-
-
-
