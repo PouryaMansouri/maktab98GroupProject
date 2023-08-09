3b78cdc3ffae52151c354bbf667d97efe8c443fc
mabrishami00 <mabrishami78@yahoo.com>
2023-08-09 14:25:53 +0330

    feat: add widget and initial value for cartaddform

--- orders/forms.py
+++ orders/forms.py
@@ -2,4 +2,9 @@ from django import forms
 
 
 class CartAddForm(forms.Form):
-    quantity=forms.IntegerField(min_value=0 , max_value=9 , label='Quantity')
+    quantity = forms.IntegerField(
+        min_value=0,
+        max_value=9,
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
+        initial=1
+    )
