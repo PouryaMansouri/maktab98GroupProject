3eb29febb1d274f84acbc34fe8a9e597f7ae26ba
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-09 22:10:22 +0330

    refactor: table_name deleted from Order

--- orders/models.py
+++ orders/models.py
@@ -2,19 +2,18 @@ from django.db import models
 from cafe.models import Product
 from accounts.models import Personnel, Customer
 
+
 class Table(models.Model):
     table_name = models.CharField(max_length=255, null=True, blank=True)
     table_number = models.IntegerField(null=True)
 
 
 class Order(models.Model):
-    STATUS_FIELDS = [('p', 'Pending'),('a','Aaccepted'),('r','Rejected')]
-    table_name = models.IntegerField(null=True)
+    STATUS_FIELDS = [("p", "Pending"), ("a", "Aaccepted"), ("r", "Rejected")]
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True, null=True)
-    status = models.CharField(max_length=1, choices= STATUS_FIELDS, default= 'p')
+    status = models.CharField(max_length=1, choices=STATUS_FIELDS, default="p")
     paid = models.BooleanField(default=False)
-
 
     # Foreign keys
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
@@ -24,7 +23,6 @@ class Order(models.Model):
     def get_total_price(self):
         return sum(item.get_cost() for item in self.orderitem_set.all())
 
-
     def __str__(self) -> str:
         return f"{self.table_name} || {self.status} || {self.create_time}"
 
@@ -32,14 +30,13 @@ class Order(models.Model):
 class OrderItem(models.Model):
     quantity = models.IntegerField(default=1)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    
+
     # Foreign keys
-    order = models.ForeignKey(Order, on_delete= models.CASCADE)
-    product = models.ForeignKey(Product, on_delete= models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.order.table_name} || {self.product} || {self.quantity}"
 
     def get_cost(self):
         return self.price * self.quantity
-
