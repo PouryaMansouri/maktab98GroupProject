6ccb80273a0307deb27f7a79d807b57e7e3a534a
mabrishami00 <mabrishami78@yahoo.com>
2023-08-09 14:49:16 +0330

    feat: additem to cart view

--- orders/views.py
+++ orders/views.py
@@ -1,48 +1,34 @@
-from django.shortcuts import render, get_object_or_404, redirect 
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .cart import Cart
 from cafe.models import Product
-from .forms import CartAddForm
+from .forms import CartAddForm, CustomerForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Order , OrderItem
+from .models import Order, OrderItem, Table
+from accounts.models import Customer
 
 
 class CartView(View):
     def get(self, request):
-        cart=Cart(request)
-        return render(request, 'orders/cart.html' , {'cart':cart})
-
-
+        cart = Cart(request)
+        context = {"cart": cart}
+        return render(request, "orders/cart.html", context)
 
 
 class CartAddView(View):
     def post(self, request, product_id):
         cart = Cart(request)
         product = get_object_or_404(Product, id=product_id)
-        form = CartAddForm(request.POST) 
+        form = CartAddForm(request.POST)
         if form.is_valid():
-            cart.add(product,form.cleaned_data['quantity'])
-        return redirect('orders:cart')
+            cart.add(product, form.cleaned_data["quantity"])
+            response = cart.save("orders:cart")
+        return response
+        # return redirect('orders:cart')
 
 
-class CartRemoveView(View):
-    def get(self,request , product_id):
-        cart=Cart(request)
-        product=get_object_or_404(Product , id=product_id)
-        cart.remove(product)
-        return redirect('orders:cart')
-    
-
-class OrderDetailView(LoginRequiredMixin , View):
-    def get(self,request,order_id):
-        order =get_object_or_404(Order, id=order_id)
-        return render(request , 'orders/order.html',{'order':order})
 
 
-class OrderCreateView(LoginRequiredMixin , View):
-    def get(self,request):
-        cart=Cart(request)
-        order=Order.objects.create(user=request.user)
-        for item in cart:
-            OrderItem.objects.create( product=item['product'] , price=float(item['price']) , quantity=item['quantity'])
-        return redirect('orders:order_detail',)
+
+
+        
