0d908365476161e351ac374dc68f0d39821b9bd9
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-09 22:04:15 +0330

    refactor: extra views deleted

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
 from .views import Menu
-from .views import HomeView ,ProductDetail , ProductDetail2, SearchView
+from .views import SearchView
 
-app_name = 'cafe'
+app_name = "cafe"
 
 urlpatterns = [
-    path('', Menu , name="menu"),
-    path('home/', HomeView.as_view(), name='home'),
-    path('search_results/', SearchView.as_view(), name='search_results'),
-    path('product/' , ProductDetail.as_view() , name="product"),
-    path('product_detail/<int:pk>/', ProductDetail2.as_view() , name="product_detail"),
-    
-    
+    path("", Menu, name="menu"),
+    path("search_results/", SearchView.as_view(), name="search_results"),
 ]
--- cafe/views.py
+++ cafe/views.py
@@ -4,6 +4,7 @@ from .models import Product, Category
 from orders.forms import CartAddForm
 from django.db.models import Q
 
+
 def Menu(request):
     all_categories = Category.objects.all()
     all_products = Product.objects.all()
@@ -15,13 +16,6 @@ def Menu(request):
     }
     return render(request, "cafe/home.html", context)
 
-class HomeView(View):
-    def get(self, request):
-        return render(request, "cafe/home.html")
-
-    def post(self, request):
-        return render(request, "cafe/home.html")
-
 
 class SearchView(View):
     def get(self, request):
@@ -30,15 +24,3 @@ class SearchView(View):
             Q(name__icontains=searched) | Q(description__icontains=searched)
         ).distinct()
         return render(request, "cafe/search_results.html", {"results": results})
-
-
-class ProductDetail(View):
-    def get(self, request):
-        products = Product.objects.filter(is_available=True)
-        return render(request, "cafe/product.html", {"products": products})
-
-
-class ProductDetail2(View):
-    def get(self, request, pk):
-        productd = Product.objects.get(pk=pk)
-        return render(request, "cafe/product_detail.html", {"productd": productd})
