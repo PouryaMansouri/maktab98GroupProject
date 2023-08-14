5e8469b3313ca9e3bb9d4c17a40c39b231ebf429
mabrishami00 <mabrishami78@yahoo.com>
2023-08-14 19:30:30 +0330

    chore: add dashboard urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("login/", views.UserLoginView.as_view(), name="login"),
     path("logout/", views.UserLogoutView.as_view(), name="logout"),
     path("verify_personnel/", views.UserVerifyView.as_view(), name="verify_personnel"),
+    path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
+    path("sales_dashboard/", views.SalesDashboardView.as_view(), name="sales_dashboard"),
     path("manage_orders/", views.ManageOrders.as_view(), name="manage_orders"),
 ]
