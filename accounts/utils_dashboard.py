871c9c8460a7a4a3d1a0c62dad0d875820f9bf40
mabrishami00 <mabrishami78@yahoo.com>
2023-08-15 08:24:24 +0330

    feat:  managing orders in different intervals

--- accounts/utils_dashboard.py
+++ accounts/utils_dashboard.py
@@ -63,3 +63,45 @@ class MostSellerProducts:
 
     def to_json(self, products_dict):
         return json.dumps(products_dict)
+
+
+class OrdersManager:
+    def __init__(self):
+        self.orders = Order.objects.all().order_by("-create_time")
+        self.paid_orders = Order.objects.filter(paid=True).order_by("-create_time")
+
+    def this_year_orders(self):
+        current_year = datetime.datetime.now().year
+        this_year_orders = self.paid_orders.filter(created_at__year=current_year)
+        return this_year_orders
+
+    def this_month_orders(self):
+        current_month = datetime.datetime.now().month
+        this_month_orders = self.paid_orders.filter(created_at__month=current_month)
+        return this_month_orders
+
+    def this_week_orders(self):
+        current_date = datetime.now().date()
+        start_of_week = current_date - datetime.timedelta(days=current_date.weekday())
+        this_week_orders = self.paid_orders.objects.filter(
+            created_at__date__gte=start_of_week
+        )
+        return this_week_orders
+
+    def today_orders(self):
+        current_date = datetime.now().date()
+        today_orders = self.paid_orders.objects.filter(created_at__date=current_date)
+        return today_orders
+
+    def orders_with_costs(self):
+        total_price = []
+        for order in self.orders:
+            total_price.append(order.get_total_price())
+        orders_with_costs = zip(self.orders, total_price)
+        return orders_with_costs
+
+    def total_sales(self):
+        return sum(paid_order.get_total_price() for paid_order in self.paid_orders)
+
+    def count_orders(self):
+        return self.paid_orders.count()
