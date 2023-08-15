939ce9b6cf7ad16c381c49045547bd075cf6faac
mabrishami00 <mabrishami78@yahoo.com>
2023-08-15 08:49:12 +0330

    feat: add last day of last month method

--- accounts/utils_dashboard.py
+++ accounts/utils_dashboard.py
@@ -14,8 +14,8 @@ class DateVars:
     def get_first_day_current_month(self):
         return self.current_date.replace(days=1)
     
-
-
+    def get_last_day_clast_month(self):
+        return self.current_date.replace(days=1) - datetime.timedelta(days=1)
 
 class MostSellerProducts:
     def most_seller_products_all(self, number):
