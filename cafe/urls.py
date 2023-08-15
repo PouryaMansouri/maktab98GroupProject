dddc0edf61430fd1f26f33c666f43433c90918e0
AnahitaDzl <anahita.dozali@gmail.com>
2023-08-15 04:54:43 +0330

    feat: product detail url added

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SearchView, HomeView
+from .views import SearchView, HomeView , ProductDetailView
 
 app_name = "cafe"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("search_results/", SearchView.as_view(), name="search_results"),
+    path("product_detail/<int:pk>", ProductDetailView.as_view(), name="product_detail")
 ]
