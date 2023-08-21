6ea2dbeecf8c0908209407afec2c48a4131a3083
BehnamGolmohammadi <gt.bg.78@gmail.com>
2023-08-22 01:33:16 +0330

    fix: ignore some culomns when export table

--- accounts/static/accounts/assets/vendor/datatables/js/data-table.js
+++ accounts/static/accounts/assets/vendor/datatables/js/data-table.js
@@ -1,9 +1,9 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     'use strict';
 
     if ($("table.first").length) {
 
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('table.first').DataTable();
         });
     }
@@ -12,22 +12,88 @@ jQuery(document).ready(function($) {
 
     if ($("table.second").length) {
 
-        $(document).ready(function() {
+        $(document).ready(function () {
             var table = $('table.second').DataTable({
-                lengthChange: false,
-                buttons: ['copy', 'excel', 'pdf', 'print', 'colvis']
+                lengthChange: true,
+                // buttons: ['copy', 'excel', 'pdf', 'print', 'colvis']
+                buttons: [
+                    {
+                        extend: 'copy',
+                        exportOptions: {
+                            columns: ':visible:not(.no-export)'
+                        }
+                    },
+                    {
+                        extend: 'excel',
+                        exportOptions: {
+                            columns: ':visible:not(.no-export)'
+                        }
+                    },
+                    {
+                        extend: 'pdf',
+                        exportOptions: {
+                            columns: ':visible:not(.no-export)'
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        exportOptions: {
+                            columns: ':visible:not(.no-export)'
+                        }
+                    },
+                    {
+                        extend: 'colvis',
+                        columns: ':not(.no-export)'
+                    }
+                ],
+                columnDefs: [
+                    {
+                      targets: [6, 7, 5], // Specify the column indexes to exclude
+                      searchable: false,
+                      export: false
+                    }
+                ]
             });
-
-            table.buttons().container()
-                .appendTo('#example_wrapper .col-md-6:eq(0)');
+            table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
         });
     }
+            //  ================================================== //
+            //     filter by date
+            // ================================================== //
+
+
+
+            // $('.filter-toggle').on('click', function () {
+            //     $('.filter-content').toggle();
+            // });
+
+            // $('#filterButton').on('click', function () {
+            //     var startDate = $('#startDateInput').val();
+            //     var endDate = $('#endDateInput').val();
+            //     var formattedStartDate = formatDate(startDate);
+            //     var formattedEndDate = formatDate(endDate);
+            
+            //     table.columns(2).search(formattedStartDate + ' to ' + formattedEndDate).draw();
+            // });
+            
+            // function formatDate(date) {
+            //     var formattedDate = moment(date, 'MMM. DD, YYYY, h:mm a').format('YYYY-MM-DD');
+            //     return formattedDate;
+            // }
+
+            // ================================================== //
+            //     end filter by date
+            // ================================================== //
+
+
+
+
 
 
     if ($("#example2").length) {
 
-        $(document).ready(function() {
-            $(document).ready(function() {
+        $(document).ready(function () {
+            $(document).ready(function () {
                 var groupColumn = 2;
                 var table = $('#example2').DataTable({
                     "columnDefs": [
@@ -37,12 +103,12 @@ jQuery(document).ready(function($) {
                         [groupColumn, 'asc']
                     ],
                     "displayLength": 25,
-                    "drawCallback": function(settings) {
+                    "drawCallback": function (settings) {
                         var api = this.api();
                         var rows = api.rows({ page: 'current' }).nodes();
                         var last = null;
 
-                        api.column(groupColumn, { page: 'current' }).data().each(function(group, i) {
+                        api.column(groupColumn, { page: 'current' }).data().each(function (group, i) {
                             if (last !== group) {
                                 $(rows).eq(i).before(
                                     '<tr class="group"><td colspan="5">' + group + '</td></tr>'
@@ -55,7 +121,7 @@ jQuery(document).ready(function($) {
                 });
 
                 // Order by the grouping
-                $('#example2 tbody').on('click', 'tr.group', function() {
+                $('#example2 tbody').on('click', 'tr.group', function () {
                     var currentOrder = table.order()[0];
                     if (currentOrder[0] === groupColumn && currentOrder[1] === 'asc') {
                         table.order([groupColumn, 'desc']).draw();
@@ -78,7 +144,7 @@ jQuery(document).ready(function($) {
     }
     if ($("#example4").length) {
 
-        $(document).ready(function() {
+        $(document).ready(function () {
             var table = $('#example4').DataTable({
                 fixedHeader: true
             });
