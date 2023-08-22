3493fb6eb1139c1b33a8b362c0596f2f703da957
HosseinTahami <hosseintahami2000@gmail.com>
2023-08-22 15:41:51 +0330

    feat: Test for CartVie

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory, Client
+from django.urls import reverse
+from .views import CartView
+from .cart import Cart
 
-# Create your tests here.
+
+class CartViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_cart_view(self):
+        response = self.client.get(reverse("orders:cart"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "orders/cart.html")
+        self.assertIsInstance(response.context["cart"], Cart)
